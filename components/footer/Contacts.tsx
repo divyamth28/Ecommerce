6dbdad304b34395b45478122b8e97f772b9cc1e3
divyamth28 <134126385+divyamth28@users.noreply.github.com>
2023-05-20 12:03:17 +0530

    Add files via upload

---
+++ components/footer/Contacts.tsx
@@ -0,0 +1,30 @@
+import {faWhatsapp} from '@fortawesome/free-brands-svg-icons/faWhatsapp';
+import {faClock} from '@fortawesome/free-solid-svg-icons/faClock';
+import {faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons/faMapMarkerAlt';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+export default function FooterContacts() {
+	return (
+		<>
+			<h3 className='page-footer__header'>Contact Us</h3>
+			<p className='page-footer__icon-w-link'>
+				<span className='icon'>
+					<FontAwesomeIcon icon={faWhatsapp}/>
+				</span>
+				<a className='link' href='tel:+91-0000000000'>+91 0000000000</a>
+			</p>
+			<p className='page-footer__icon-w-link'>
+				<span className='icon'>
+					<FontAwesomeIcon icon={faMapMarkerAlt}/>
+				</span>
+				<a className='link' href='#'>Clement Town, Dehradun- 248002</a>
+			</p>
+			<p className='page-footer__icon-w-link'>
+				<span className='icon'>
+					<FontAwesomeIcon icon={faClock}/>
+				</span>
+				9:00am &mdash; 10:00pm
+			</p>
+		</>
+	);
+}
